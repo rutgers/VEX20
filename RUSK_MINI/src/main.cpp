22529bbcbe8c3911a5f84a3d90a92f09f4ef9bf0
Hardware-Man <km1233@scarletmail.rutgers.edu>
2020-02-21 20:24:31 -0500

    Add files via upload

---
+++ RUSK_MINI/src/main.cpp
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Adjusting Speed                                           */
+/*                                                                            */
+/*    This program demonstrates how to change the speed of a drivetrains      */
+/*    drive and turn actions.                                                 */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// LeftIN               motor         14              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // Drives robot forward 6 inches at the default 50% velocity
+  
+}
